3e0727a536b413fca7740284d133ce70c03c1604
frmfelipemaia <felipermaia@proton.me>
2024-09-02 10:55:52 -0300

    Exercicios 2 do dia 21 concluído

--- exercicio_dia_21_08.ts
+++ exercicio_dia_21_08.ts
@@ -16,7 +16,6 @@ function exibirProdutoComPagamento(produto: Produto, pagamento: FormaPagamento):
 const produtoExemplo: Produto = { nome: 'PS5', preco: 5000, categoria: 'Eletrônicos' };
 
 const formaPagamentoExemplo: FormaPagamento = 'cartão';
-
 console.log(exibirProdutoComPagamento(produtoExemplo, formaPagamentoExemplo));
 
 
@@ -29,3 +28,50 @@ type Empregado = {
     empresa: string;
     salario: number;
 };
+
+// Exercício 2
+
+class Funcionario {
+    constructor(public nome: string, public cargo: string, public salario: number) { }
+
+    descricao(): string {
+        return `Funcionário: ${this.nome}, Cargo: ${this.cargo}, Salário: R$${this.salario.toFixed(2)}`;
+    }
+}
+
+class Gerente extends Funcionario {
+    constructor(nome: string, cargo: string, salario: number, public departamento: string) {
+        super(nome, cargo, salario);
+    }
+
+    descricaoDetalhada(): string {
+        return `${super.descricao()}, Departamento: ${this.departamento}`;
+    }
+}
+
+const gerenteExemplo = new Gerente('Felipe', 'CEO', 120000, 'Tecnologia');
+console.log(gerenteExemplo.descricaoDetalhada());
+
+// Exercício 2.2
+
+class ContaBancaria {
+    constructor(public titular: string, public saldo: number) { }
+
+    exibirSaldo(): string {
+        return `Titular: ${this.titular}, Saldo: R$${this.saldo.toFixed(2)}`;
+    }
+}
+class ContaCorrente extends ContaBancaria {
+    constructor(titular: string, saldo: number, public limiteCredito: number) {
+        super(titular, saldo);
+    }
+
+    exibirSaldo(): string {
+        const saldoTotal = this.saldo + this.limiteCredito;
+        return `${super.exibirSaldo()}, Saldo Total com Crédito: R$${saldoTotal.toFixed(2)}`;
+    }
+}
+
+const contaCorrenteExemplo = new ContaCorrente('Felipe', 10000, 3000);
+console.log(contaCorrenteExemplo.exibirSaldo());
+
